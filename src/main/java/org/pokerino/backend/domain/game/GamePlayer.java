06c3944997c2c245847ba1392fac1dc968ca234f
Bollhöfer, Lukas - D0342970 <lukas.bollhoefer@dm.de>
2025-02-10 10:09:50 +0100

    implement GamePlayer

--- src/main/java/org/pokerino/backend/domain/game/GamePlayer.java
+++ src/main/java/org/pokerino/backend/domain/game/GamePlayer.java
@@ -1,13 +1,23 @@
 package org.pokerino.backend.domain.game;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
 @Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class GamePlayer {
-    private int total;
-    private int bet;
-    private String[] hand;
+    final long userId;
+    @Setter int total;
+    @Setter int bet;
+    String[] hand;
     // Todo: Add fields that are needed for UI here...
+
+    public GamePlayer(long userId, int total) {
+        this.userId = userId;
+        this.total = total;
+    }
 
     public void setHand(String... hand) {
         if (hand.length != 2) {
