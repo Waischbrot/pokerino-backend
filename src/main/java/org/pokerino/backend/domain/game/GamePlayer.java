04ac195583ef2096d39fcdcf49d0e94472ed7e33
Bollhöfer, Lukas - D0342970 <lukas.bollhoefer@dm.de>
2025-02-10 08:46:24 +0100

    implement GamePlayer

--- src/main/java/org/pokerino/backend/domain/game/GamePlayer.java
+++ src/main/java/org/pokerino/backend/domain/game/GamePlayer.java
@@ -1,4 +1,18 @@
 package org.pokerino.backend.domain.game;
 
+import lombok.Getter;
+
+@Getter
 public class GamePlayer {
+    private int total;
+    private int bet;
+    private String[] hand;
+    // Todo: Add fields that are needed for UI here...
+
+    public void setHand(String... hand) {
+        if (hand.length != 2) {
+            throw new IllegalStateException("Too many cards present!");
+        }
+        this.hand = hand;
+    }
 }
