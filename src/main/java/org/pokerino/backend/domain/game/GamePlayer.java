c2addc2317a5d4db8adfb2b08e2a90df48c9efcf
Bollhöfer, Lukas - D0342970 <lukas.bollhoefer@dm.de>
2025-02-12 10:23:18 +0100

    check if a player is dead or folded, add comments and add GamePlayer#isAllIn

--- src/main/java/org/pokerino/backend/domain/game/GamePlayer.java
+++ src/main/java/org/pokerino/backend/domain/game/GamePlayer.java
@@ -9,14 +9,20 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class GamePlayer {
     final long userId;
-    @Setter int total;
-    @Setter int bet;
+    @Setter int total; // How many chips does this player have left
+    @Setter int bet; // How much of his chips are currently in the pot
     String[] hand;
+    @Setter boolean folded; // Whenever a player folds, this is set to true.
+    @Setter boolean dead; // Has this player already lost?
     // Todo: Add fields that are needed for UI here...
 
     public GamePlayer(long userId, int total) {
         this.userId = userId;
         this.total = total;
+    }
+
+    public boolean isAllIn() {
+        return this.total == 0;
     }
 
     public void setHand(String... hand) {
