56376221aae3f434cf33d21fca346d825f015b7e
Bollhöfer, Lukas - D0342970 <lukas.bollhoefer@dm.de>
2025-02-19 14:59:36 +0100

    begin on AuthenticationController

--- src/main/java/org/pokerino/backend/adapter/in/AuthenticationController.java
+++ src/main/java/org/pokerino/backend/adapter/in/AuthenticationController.java
@@ -3,8 +3,13 @@ package org.pokerino.backend.adapter.in;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.pokerino.backend.adapter.in.dto.RegisterUserDto;
 import org.pokerino.backend.application.port.in.AuthenticationUseCase;
 import org.pokerino.backend.application.port.in.JWTUseCase;
+import org.pokerino.backend.domain.user.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +21,8 @@ public class AuthenticationController {
     JWTUseCase jwtUseCase;
     AuthenticationUseCase authenticationUseCase;
 
+    @PostMapping("/signup")
+    public ResponseEntity<User> signup(@RequestBody RegisterUserDto registerUserDto) {
 
+    }
 }
