3aabdcf87e0ed96cc92e7fa12dca2b56f4bd4dda
Bollhöfer, Lukas - D0342970 <lukas.bollhoefer@dm.de>
2025-02-19 14:26:05 +0100

    init AuthenticationController for mapping auth requests

---
+++ src/main/java/org/pokerino/backend/adapter/in/AuthenticationController.java
@@ -0,0 +1,20 @@
+package org.pokerino.backend.adapter.in;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.pokerino.backend.application.port.in.AuthenticationUseCase;
+import org.pokerino.backend.application.port.in.JWTUseCase;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+public class AuthenticationController {
+    JWTUseCase jwtUseCase;
+    AuthenticationUseCase authenticationUseCase;
+
+
+}
