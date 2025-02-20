f351e44b8f3d025741fc41fe4cf8349e5ea98041
Bollhöfer, Lukas - D0342970 <lukas.bollhoefer@dm.de>
2025-02-20 10:32:21 +0100

    implement AuthenticationController, though is still missing some parts!

--- src/main/java/org/pokerino/backend/adapter/in/AuthenticationController.java
+++ src/main/java/org/pokerino/backend/adapter/in/AuthenticationController.java
@@ -23,18 +23,24 @@ public class AuthenticationController {
 
     @PostMapping("/signup")
     public ResponseEntity<User> signup(@RequestBody RegisterUserDto registerUserDto) {
-
+        final User registeredUser = this.authenticationUseCase.signup(registerUserDto);
+        // Todo: Handle errors & other responses - for example what if account with email already exists? Or other things!
+        return ResponseEntity.ok(registeredUser);
     }
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto) {
-
+        final User authenticatedUser = this.authenticationUseCase.authenticate(loginUserDto);
+        final String jwtToken = this.jwtUseCase.generateToken(authenticatedUser);
+        final LoginResponse loginResponse = new LoginResponse(jwtToken, this.jwtUseCase.getExpirationTime());
+        return ResponseEntity.ok(loginResponse);
     }
 
     @PostMapping("/verify")
     public ResponseEntity<?> verifyUser(@RequestBody VerifyUserDto verifyUserDto) {
         try {
-
+            this.authenticationUseCase.verifyUser(verifyUserDto);
+            return ResponseEntity.ok("Account verified successfully");
         } catch (RuntimeException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
@@ -43,7 +49,8 @@ public class AuthenticationController {
     @PostMapping("/resend")
     public ResponseEntity<?> resendVerificationCode(@RequestParam String email) {
         try {
-
+            this.authenticationUseCase.resendVerificationCode(email);
+            return ResponseEntity.ok("Verification code sent");
         } catch (RuntimeException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
