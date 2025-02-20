95fd92a97bda19ac1ebb197084bc55caa523067b
Bollhöfer, Lukas - D0342970 <lukas.bollhoefer@dm.de>
2025-02-20 10:17:49 +0100

    User object with fields + can be persisted

--- src/main/java/org/pokerino/backend/domain/user/User.java
+++ src/main/java/org/pokerino/backend/domain/user/User.java
@@ -1,43 +1,68 @@
 package org.pokerino.backend.domain.user;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.List;
 
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    @Column(unique = true, nullable = false)
+    String username;
+    @Column(unique = true, nullable = false)
+    String email;
+    @Column(nullable = false)
+    String password;
+
+    @Column(name = "verification_code")
+    String verificationCode;
+    @Column(name = "verification_expiration")
+    LocalDateTime verificationCodeExpiresAt;
+    boolean enabled;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {}
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-
-    }
-
-    @Override
-    public String getPassword() {
-
-    }
-
-    @Override
-    public String getUsername() {
-
+        return List.of();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-
+        return enabled;
     }
 }
