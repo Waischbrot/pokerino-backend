53d96846cfda3833aef3a76c3cd1042e5a4709a1
Bollhöfer, Lukas - D0342970 <lukas.bollhoefer@dm.de>
2025-02-28 08:47:32 +0100

    remove auth since mailhog does not support that

--- src/main/java/org/pokerino/backend/config/MailServerConfig.java
+++ src/main/java/org/pokerino/backend/config/MailServerConfig.java
@@ -12,16 +12,12 @@ import java.util.Properties;
 public class MailServerConfig {
     @Value("${spring.mail.host}") private String emailHost;
     @Value("${spring.mail.port}") private int emailPort;
-    @Value("${spring.mail.username}") private String emailUsername;
-    @Value("${spring.mail.password}") private String emailPassword;
 
     @Bean
     public JavaMailSender javaMailSender() {
         final JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost(this.emailHost);
         mailSender.setPort(this.emailPort);
-        mailSender.setUsername(this.emailUsername);
-        mailSender.setPassword(this.emailPassword);
         final Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
