8504b8c3df25d331a9189cb5e4163c5afec404fb
Bollhöfer, Lukas - D0342970 <lukas.bollhoefer@dm.de>
2025-03-07 11:01:31 +0100

    add javadoc

--- src/main/java/org/pokerino/backend/domain/exception/auth/UserExistsException.java
+++ src/main/java/org/pokerino/backend/domain/exception/auth/UserExistsException.java
@@ -1,5 +1,9 @@
 package org.pokerino.backend.domain.exception.auth;
 
+/**
+ * Exception thrown when auth registration endpoint is called and there
+ * is already a user with email or username, since they have to be unique!
+ */
 public class UserExistsException extends RuntimeException {
     public UserExistsException(String message) {
         super(message);
