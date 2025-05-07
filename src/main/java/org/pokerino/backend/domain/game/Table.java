1d9ce84bebb79e29c9a3b180745723587e653b88
Maksym Lykhovych <maksym.lykhovych@bee360.com>
2025-05-07 11:37:52 +0200

    add different Tables

--- src/main/java/org/pokerino/backend/domain/game/Table.java
+++ src/main/java/org/pokerino/backend/domain/game/Table.java
@@ -10,7 +10,14 @@ import lombok.experimental.FieldDefaults;
 @Getter
 public enum Table {
     // Task: Think of 7 different tables with unique buy ins and values for players to play at!
+    LowStakes("LowStakes",10000,1000,10,20,360,30000,0),
+    BigStakes("BigStakes",100000,20000,1000,2000,360,300000,0),
+    LowStakesBlitz("LowStakesBlitz",10000,1000,10,20,180,30000,0),
+    BigStakesBlitz("BigStakesBlitz",1000000,20000,1000,2000,360,3000000,0),
+    LowStakesTwoWinners("LowStakesTwoWinners",10000,1000,10,20,360,20000,10000),
+    BigStakesTwoWinners("BigStakesTwoWinners",1000000,20000,1000,2000,360,200000,100000)
     ;
+
 
     String name; // The name of the table
     int buyIn; // How much does it cost to join the table
