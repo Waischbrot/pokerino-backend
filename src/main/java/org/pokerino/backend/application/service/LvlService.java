0d2619717755a3db26e8509f39a22814dddb7c3a
Maksym Lykhovych <maksym.lykhovych@bee360.com>
2025-05-08 12:38:59 +0200

    add LEvelService

--- src/main/java/org/pokerino/backend/application/service/LvlService.java
+++ src/main/java/org/pokerino/backend/application/service/LvlService.java
@@ -1,5 +1,23 @@
+package org.pokerino.backend.application.service;
+
 import org.pokerino.backend.application.port.in.LevelUseCase;
+import org.pokerino.backend.domain.user.User;
 
 public class LvlService implements LevelUseCase{
+
+    public int calculateLevel(long exp){
+        int level = 0;
+        int requiredExp =  100;
+        while(exp>=requiredExp){
+            level++;
+            requiredExp*=2;
+        }
+        return level;
+    }
+
+
+    public void addExperience(User user, long exp){
+        user.setExperience(user.getExperience()+exp);
+    }
     
 }
