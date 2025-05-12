ba7530e43d8dce512a9f3e9f5cffb0522283a3b9
Bollhöfer, Lukas - D0342970 <lukas.bollhoefer@dm.de>
2025-05-12 10:39:55 +0200

    return User object on login

--- src/main/java/org/pokerino/backend/adapter/in/response/LoginResponse.java
+++ src/main/java/org/pokerino/backend/adapter/in/response/LoginResponse.java
@@ -1,6 +1,6 @@
 package org.pokerino.backend.adapter.in.response;
 
 public record LoginResponse(
-        String username,
-        String token
+        String token,
+        UserResponse user
 ) {}
