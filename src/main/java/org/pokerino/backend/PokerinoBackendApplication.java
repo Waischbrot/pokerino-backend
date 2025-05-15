c71dc8222ce323e26862c7705019e84602069b9e
Bollhöfer, Lukas - D0342970 <lukas.bollhoefer@dm.de>
2025-05-15 13:27:04 +0200

    make swagger use https

--- src/main/java/org/pokerino/backend/PokerinoBackendApplication.java
+++ src/main/java/org/pokerino/backend/PokerinoBackendApplication.java
@@ -1,9 +1,14 @@
 package org.pokerino.backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {
+				@Server(url = "/", description = "Default Server URL")
+})
 public class PokerinoBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PokerinoBackendApplication.class, args);
