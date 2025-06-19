c28336e3278eddb16494b6c8f4cfc103fdbfee45
Maksym Lykhovych <maksym.lykhovych@bee360.com>
2025-06-19 22:46:52 +0200

    comment logic of game moves(be careful, comments may be disgusting)

--- src/main/java/org/pokerino/backend/application/service/GameMovesService.java
+++ src/main/java/org/pokerino/backend/application/service/GameMovesService.java
@@ -12,12 +12,14 @@ public class GameMovesService implements GameMovesUseCase {
 
     GameInMemoryRepository gameInMemoryRepository;
 
+    //fold, just remove player from players, play stay in participants
     @Override
     public void fold(long playerId) {
         PokerGame game = gameInMemoryRepository.getGame(playerId).get();
         game.removePlayerFromRound(playerId);
     }
 
+    // call, if player has enough chips, when not automatic allIn(is it right logic or we should throw exception in those situations?)
     @Override
     public void call(long playerId) {
         PokerGame game = gameInMemoryRepository.getGame(playerId).get();
@@ -36,6 +38,9 @@ public class GameMovesService implements GameMovesUseCase {
         else throw new CallIsNotNeededException("Player" + playerId+ " has nothing to call");
     }
 
+    //Player raise right(call previous raise, if it was, + bet a previous raise), 
+    //when not or not enough chips to raise, then throw exception
+    //also memorize this raise to check if the next possible raise in this round will be right
     @Override
     public void raise(long playerId, int numberOfChips) {
         PokerGame game = gameInMemoryRepository.getGame(playerId).get();
@@ -53,6 +58,7 @@ public class GameMovesService implements GameMovesUseCase {
         else throw new WrongRaiseException("The raise of player" + playerId + "with " + numberOfChips + "isn't enough. It should be at least call + double of last raise." );
     }
 
+    //just put all total balance in
     @Override
     public void allIn(long playerId) {
         PokerGame game = gameInMemoryRepository.getGame(playerId).get();
@@ -60,6 +66,7 @@ public class GameMovesService implements GameMovesUseCase {
         player.bet(player.getTotal());
     }
 
+    //check, if there was no raise
     @Override
     public void check(long playerId) {
         PokerGame game = gameInMemoryRepository.getGame(playerId).get();
