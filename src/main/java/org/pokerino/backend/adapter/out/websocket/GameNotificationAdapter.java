b8ced25b88e33a61a0648c727948ec929bdf499b
Bollhöfer, Lukas - D0342970 <lukas.bollhoefer@dm.de>
2025-06-23 10:45:11 +0200

    finish posting notifications to websockets

--- src/main/java/org/pokerino/backend/adapter/out/websocket/GameNotificationAdapter.java
+++ src/main/java/org/pokerino/backend/adapter/out/websocket/GameNotificationAdapter.java
@@ -4,8 +4,14 @@ import lombok.AccessLevel;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.pokerino.backend.adapter.out.websocket.message.PlayerJoinMessage;
-import org.pokerino.backend.adapter.out.websocket.message.PlayerLeaveMessage;
+import org.pokerino.backend.adapter.out.websocket.message.FinishGameMessage;
+import org.pokerino.backend.adapter.out.websocket.message.NextBetMessage;
+import org.pokerino.backend.adapter.out.websocket.message.NextRoundMessage;
+import org.pokerino.backend.adapter.out.websocket.message.TurnMessage;
+import org.pokerino.backend.adapter.out.websocket.message.log.ActionMessage;
+import org.pokerino.backend.adapter.out.websocket.message.log.BlindMessage;
+import org.pokerino.backend.adapter.out.websocket.message.log.PlayerJoinMessage;
+import org.pokerino.backend.adapter.out.websocket.message.log.PlayerLeaveMessage;
 import org.pokerino.backend.application.port.out.GameNotificationPort;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
@@ -32,6 +38,36 @@ public final class GameNotificationAdapter implements GameNotificationPort {
 
     @Override
     public void notifyPlayerJoin(String gameCode, PlayerJoinMessage message) {
+        notify(gameCode, message);
+    }
+
+    @Override
+    public void notifyBlind(String gameCode, BlindMessage message) {
+        notify(gameCode, message);
+    }
+
+    @Override
+    public void notifyAction(String gameCode, ActionMessage message) {
+        notify(gameCode, message);
+    }
+
+    @Override
+    public void notifyFinishGame(String gameCode, FinishGameMessage message) {
+        notify(gameCode, message);
+    }
+
+    @Override
+    public void notifyNextBet(String gameCode, NextBetMessage message) {
+        notify(gameCode, message);
+    }
+
+    @Override
+    public void notifyNextRound(String gameCode, NextRoundMessage message) {
+        notify(gameCode, message);
+    }
+
+    @Override
+    public void notifyTurn(String gameCode, TurnMessage message) {
         notify(gameCode, message);
     }
 }
--- src/main/java/org/pokerino/backend/application/port/out/GameNotificationPort.java
+++ src/main/java/org/pokerino/backend/application/port/out/GameNotificationPort.java
@@ -1,10 +1,22 @@
 package org.pokerino.backend.application.port.out;
 
-import org.pokerino.backend.adapter.out.websocket.message.PlayerJoinMessage;
-import org.pokerino.backend.adapter.out.websocket.message.PlayerLeaveMessage;
+import org.pokerino.backend.adapter.out.websocket.message.FinishGameMessage;
+import org.pokerino.backend.adapter.out.websocket.message.NextBetMessage;
+import org.pokerino.backend.adapter.out.websocket.message.NextRoundMessage;
+import org.pokerino.backend.adapter.out.websocket.message.TurnMessage;
+import org.pokerino.backend.adapter.out.websocket.message.log.ActionMessage;
+import org.pokerino.backend.adapter.out.websocket.message.log.BlindMessage;
+import org.pokerino.backend.adapter.out.websocket.message.log.PlayerJoinMessage;
+import org.pokerino.backend.adapter.out.websocket.message.log.PlayerLeaveMessage;
 
 public interface GameNotificationPort {
     void notifyPlayerLeave(String gameCode, PlayerLeaveMessage message);
     void notifyPlayerJoin(String gameCode, PlayerJoinMessage message);
+    void notifyBlind(String gameCode, BlindMessage message);
+    void notifyAction(String gameCode, ActionMessage message);
+    void notifyFinishGame(String gameCode, FinishGameMessage message);
+    void notifyNextBet(String gameCode, NextBetMessage message);
+    void notifyNextRound(String gameCode, NextRoundMessage message);
+    void notifyTurn(String gameCode, TurnMessage message);
 
 }
