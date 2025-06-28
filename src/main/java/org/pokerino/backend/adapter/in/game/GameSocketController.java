0f6eecfba2c336d2472e7db45df83ff50cadcf84
Bollhöfer, Lukas - D0342970 <lukas.bollhoefer@dm.de>
2025-06-28 19:57:48 +0200

    inbound controller for calling turns

--- src/main/java/org/pokerino/backend/adapter/in/game/GameSocketController.java
+++ src/main/java/org/pokerino/backend/adapter/in/game/GameSocketController.java
@@ -1,12 +1,48 @@
 package org.pokerino.backend.adapter.in.game;
 
+import lombok.AccessLevel;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.pokerino.backend.adapter.in.dto.game.TurnMessage;
+import org.pokerino.backend.application.port.in.game.TurnUseCase;
+import org.pokerino.backend.application.port.out.LoadGamePort;
+import org.pokerino.backend.domain.game.GamePlayer;
+import org.pokerino.backend.domain.game.GameState;
+import org.pokerino.backend.domain.game.PokerGame;
+import org.pokerino.backend.domain.outbound.exception.BadRequestException;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class GameSocketController {
+    TurnUseCase turnUseCase;
+    LoadGamePort loadGamePort;
 
-    // Maybe we need to split this into Inbound and Outbound controller
-    // -> Outbound controller is for sending messages from the service layer to all clients
-    // The new name would be InboundSocketController for this
+    @MessageMapping("/game/turn")
+    public void handleTurn(final TurnMessage message) {
+        final String username = getUsername();
+        final PokerGame game = loadGamePort.getUserGame(username)
+                .orElseThrow(() -> new BadRequestException("No game found for user: " + username));
+        if (game.getState() != GameState.IN_ROUND) {
+            throw new BadRequestException("Game is not in a valid state for turns: " + game.getState());
+        }
+        final GamePlayer player = game.getPlayer(username);
 
+        switch (message.action()) {
+            case FOLD -> turnUseCase.fold(game, player);
+            case CHECK -> turnUseCase.check(game, player);
+            case CALL -> turnUseCase.call(game, player);
+            case RAISE -> turnUseCase.raise(game, player, message.chips());
+            case ALL_IN -> turnUseCase.allIn(game, player);
+        }
+    }
+
+    @NonNull
+    private String getUsername() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
 }
